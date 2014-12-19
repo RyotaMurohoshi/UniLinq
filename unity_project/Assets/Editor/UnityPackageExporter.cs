bb76ca96bf199e33a317c9ec0851c5911183efab
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2014-12-20 02:00:26 +0900

    added Editor script to export package

---
+++ unity_project/Assets/Editor/UnityPackageExporter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UniLinq.Utility
+{
+	public class UnityPackageExporter
+	{
+		[MenuItem("Assets/Export UniLinq")]
+		public static void ExportUnityPackage ()
+		{
+			Debug.Log ("Export UniLinq");
+			AssetDatabase.ExportPackage ("Assets/UniLinq", "UniLinq.unitypackage", ExportPackageOptions.Recurse);
+		}
+	}
+}
